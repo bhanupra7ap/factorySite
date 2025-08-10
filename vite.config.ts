03a86a3fad0057739cc891dab2fd8fe350a034a6
Bhanu Pratap Singh <bhanu@Bhanus-MacBook-Air.local>
2025-08-10 18:34:38 +0530

    reverted target

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/factorySite/', // Set the base path for the application
 })
