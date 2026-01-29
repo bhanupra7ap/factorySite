b3d7d9f0ae04d3c87e26672ef79e4d70d9e0fa27
Bhanu <bhanupra7aps1ngh@gmail.com>
2026-01-29 18:41:30 +0530

    remove social links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import './Footer.css';
-import { FaLinkedin, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+// import { FaLinkedin, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
